53767c731451459e69f7d8ef2d58747a92a3156a
Adhami3310 <khaleel.aladhami@gmail.com>
2023-02-06 06:09:56 -0500

    update version

--- converter/main.py
+++ converter/main.py
@@ -82,7 +82,7 @@ class ConverterApplication(Adw.Application):
                                 application_name='Converter',
                                 application_icon='io.gitlab.adhami3310.Converter',
                                 developer_name='Khaleel Al-Adhami',
-                                version='1.5.1',
+                                version='1.5.2',
                                 copyright='Copyright © 2022 Khaleel Al-Adhami',
                                 license_type=Gtk.License.GPL_3_0_ONLY,
                                 website='https://gitlab.com/adhami3310/Converter',
